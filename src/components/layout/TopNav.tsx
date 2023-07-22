3b1ca5a840233cf271063c36a345528a28354b20
Andrés Esguerra <afesguerra@outlook.com>
2023-07-22 12:16:59 +0200

    Branding

--- src/components/layout/TopNav.tsx
+++ src/components/layout/TopNav.tsx
@@ -1,10 +1,13 @@
 import {TopNavigation, TopNavigationProps} from "@cloudscape-design/components";
 import {useState} from "react";
 import {applyMode, Mode} from "@cloudscape-design/global-styles";
+import snorlax from '../../assets/snorlaxhead.svg'
+import github from '../../assets/github.svg'
 
 
 const identity: TopNavigationProps.Identity = {
-    title: 'Habitica Graph Portal',
+    title: 'Snorlax',
+    logo: {src: snorlax},
     href: "#",
 };
 
@@ -17,6 +20,7 @@ export const TopNav = ({showPreferences}: TopNavProps) => {
     applyMode(darkMode ? Mode.Dark : Mode.Light);
 
     return (<TopNavigation data-header={'header'} identity={identity} utilities={[
+        {type: "button", iconUrl: github, href: 'https://github.com/afesguerra/snorlax', target: '_blank'},
         {type: "button", iconName: "star-filled", onClick: () => setDarkMode(p => !p)},
         {type: "button", iconName: "settings", onClick: showPreferences},
     ]}/>);
